6e9dc54cd9b149c3f0894f802325b02b198018ae
Nixoy <john.nicholai.vigilia@gmail.com>
2024-05-11 06:41:13 +0800

    add z-index

--- components/FormInputs/SelectInputWithSearch.jsx
+++ components/FormInputs/SelectInputWithSearch.jsx
@@ -83,7 +83,7 @@ export default function SelectInputWithSearch({
           </svg>
         </button>
         {isOpen && (
-          <div className="absolute w-full mt-2 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 p-1 space-y-1">
+          <div className="absolute w-full mt-2 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 p-1 space-y-1 z-10">
             <input
               id="search-input"
               className="block w-full px-4 py-2 text-gray-800 border rounded-md border-gray-300 focus:outline-none"
