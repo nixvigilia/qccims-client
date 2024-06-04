16595152edb69c4e8ba1884d90a215e00fbb9657
Nixoy <john.nicholai.vigilia@gmail.com>
2024-06-05 03:26:20 +0800

    update signin directory

--- app/login/page.js
+++ app/login/page.js
@@ -1,5 +1,5 @@
 import React from "react";
-import LoginForm from "@/components/Auth/LoginForm";
+import LoginForm from "../../components/Auth/LoginForm";
 
 export default function Page() {
   return <LoginForm />;
